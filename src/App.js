e338f397fec665b3e699ae39fe6c4e45a9514a08
wechuli <wechulipaul@yahoo.com>
2018-12-05 15:20:00 +0300

    making boiler plate with react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       This is my content
+      </div>
+    );
+  }
+}
+
+export default App;
