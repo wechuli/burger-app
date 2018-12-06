040b69ec36e7047511fb04f700d52194b2ed2aed
Wechuli <wechulipaul@yahoo.com>
2018-12-06 12:46:09 +0300

    adding state to manage ingredient components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Auxilliary from "../../hoc/Auxilliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
   render() {
     return (
       <Auxilliary>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Auxilliary>
     );
